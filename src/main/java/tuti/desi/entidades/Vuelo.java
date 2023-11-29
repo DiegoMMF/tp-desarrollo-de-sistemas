cba754453b7fcb53407a67e4e11d64e8086dc2d9
JoaCourault <joa.courault@gmail.com>
2023-11-29 19:25:35 -0300

    Agregado el .gitignore

--- src/main/java/tuti/desi/entidades/Vuelo.java
+++ src/main/java/tuti/desi/entidades/Vuelo.java
@@ -14,7 +14,7 @@ public class Vuelo {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-
+    
     @Column(nullable = false, unique = true)
     private String numeroVuelo;
 
